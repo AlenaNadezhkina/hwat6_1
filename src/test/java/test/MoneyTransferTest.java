21be20e01606917e81911cddcd7a080922e41fb9
AlenaNadezhkina <61390359+AlenaNadezhkina@users.noreply.github.com>
2025-09-21 15:45:52 +0300

    Initial commit

---
+++ src/test/java/test/MoneyTransferTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import data.DataHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.DashboardPage;
+import page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataHelper.generateInvalidAmount;
+import static data.DataHelper.generateValidAmount;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    DataHelper.CardInfo firstCardInfo;
+    DataHelper.CardInfo secondardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+    int amount;
+
+    @BeforeEach
+    void setup() {
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authinfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authinfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authinfo);
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        firstCardInfo = DataHelper.getFirstCardInfo();
+        secondardInfo = DataHelper.getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        secondCardBalance = dashboardPage.getCardBalance(secondardInfo);
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        assertAll(
+                () -> dashboardPage.checkCardBalance(firstCardInfo, expectedBalanceFirstCard),
+                () -> dashboardPage.checkCardBalance(secondardInfo, expectedBalanceSecondCard));
+    }
+}
